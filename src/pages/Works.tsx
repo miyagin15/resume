878e66567d8fc889149868484cc1d6b1fed3ee92
miyagin15 <47261829+miyagin15@users.noreply.github.com>
2023-03-07 23:57:26 +0900

    add: skillや資格、経験をsonで管理する

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,7 +1,17 @@
 import Footer from '../style/Footer'
 import Header from '../style/Header'
+import works from '../data/works.json'
 
 function Works() {
+  const hobbyList = works.hobbies.map((hobby) =>
+    <li>{hobby}</li>
+  );
+  const studentList = works.students.map((student) =>
+    <li>{student}</li>
+  );
+  const employmentList = works.employments.map((employment) =>
+    <li>{employment}</li>
+  );
   return (
     <div style={{ textAlign: "center", justifyContent: "center", backgroundColor: "#f5f5ff" }}>
       <Header />
@@ -9,19 +19,19 @@ function Works() {
         <h2 id="保有スキル">職務経歴詳細</h2>
         <div style={{ display: "flex", alignItems: "center", flexDirection: "column", listStyle: "disc", paddingLeft: "5px" }}>
           <ul style={{ textAlign: "left" }}>
-            <li>インターン1社2年 (LineAPI+LinePay+Stripe+Watson+Django+React)</li>
-            <li>2020年インターン3社 (Unity,Swift,AWS(ETL処理),Swift)</li>
-            <li>2021年インターン3社 (Unity,GCP,React,CloudFormation,CDK)</li>
-            <li>2021上半期 (Terraform+AWS+Azure+python)</li>
-            <li>2021下半期 (python+NoSQL+Azure+AWS+深層学習)</li>
+            {employmentList}
+          </ul>
+        </div>
+        <h2 id="保有スキル">学生時代の経験</h2>
+        <div style={{ display: "flex", alignItems: "center", flexDirection: "column", listStyle: "disc", paddingLeft: "5px" }}>
+          <ul style={{ textAlign: "left" }}>
+            {studentList}
           </ul>
         </div>
         <h2 id="保有スキル">趣味開発。リリース済みアプリ</h2>
         <div style={{ display: "flex", alignItems: "center", flexDirection: "column", listStyle: "disc", paddingLeft: "5px" }}>
           <ul style={{ textAlign: "left" }}>
-            <li>AI顔の大きさ診断アプリ (Unity+ncmb+firebase)</li>
-            <li>カロリーアプリ (react-native+gcp)</li>
-            <li>ナッツの栄養アプリ (Flutter+firebase)</li>
+            {hobbyList}
           </ul>
         </div>
       </div>
